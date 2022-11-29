b850ed113cfd46fb004b38a71c745a637e50ec57
vijayatharani99 <116818442+vijayatharani99@users.noreply.github.com>
2022-11-29 22:01:24 +0530

    Add files via upload

---
+++ string-length-for loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char s[1000];
+    int i=0;
+    printf("Enter String: ");
+    scanf("%s", s);
+    for(i = 0; s[i] != '\0'; i++);
+    printf("Length of String: %d", i);
+    return 0;
+}
